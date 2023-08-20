79065c92861ffbbc657be3ab746e80e3c45e1e2a
Nannielf <nannielf.no@gmail.com>
2023-08-20 19:41:42 +0300

    Update modul_1-2.js

--- lesson_1/modul_1-2.js
+++ lesson_1/modul_1-2.js
@@ -1,12 +1,24 @@
 //TODO: № 1 на if...else ===============================================
 // Якщо змінна a дорівнює 10, виведіть 'Вірно', інакше виведіть 'Невірно'.
 
-let a = 15;
+// let a = 15;
 
-// if (a === 10) {
-//     console.log('Вірно');
-// } else {
-//     console.log("Невірно");
-// }
+// // if (a === 10) {
+// //     console.log('Вірно');
+// // } else {
+// //     console.log("Невірно");
+// // }
 
-a === 10 ? console.log("Вірно") : console.log("Невірно");
+// a === 10 ? console.log("Вірно") : console.log("Невірно");
+
+let min = 59;
+
+if(min <= 15) {
+    console.log("1 четверть");
+} else if(min <= 30) {
+    console.log("2 четверть");
+} else if( min <= 45) {
+    console.log("3 четверть");
+} else {
+    console.log("4 четверть");
+}
