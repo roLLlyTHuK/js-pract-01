f5c7dbc7181a0df22e99a92ba4aa29bedb60f044
roLLlyTHuK <ircvipua@gmail.com>
2023-09-17 20:42:31 +0300

    task7

--- lesson_3/module_5.js
+++ lesson_3/module_5.js
@@ -92,3 +92,59 @@
 // const user1 = new User({ userName: "Olga", age: 22, numbersOfPost: 105 });
 // console.log(user1);
 // console.log(user1.getInfo());
+
+// TODO: № 7 на class-и =============================================
+// Напиши клас Notes який управляє колекцією нотаток у
+// властивості items.
+// Нотатка - це об'єкт із властивостями text і priority.
+// Додай класу статичний властивість Priopity,
+// який буде зберігати об'єкт із пріоритетами ("high" i "low").
+// Додай методи addNote(note), removeNote(noteName)
+// updatePriority(text, newPriority)
+
+// class Notes {
+//   static Priority ={
+//    high : "high",
+//    low : "low",
+//   }
+
+//   constructor () {
+//     this.items = [];
+//   }
+//   showNotes(){
+//     console.log(this.items)
+//   }
+//   addNote(note){
+//     this.items.push(note)
+//   }
+//   removeNote(noteName){
+//     const indexItem =this.items.findIndex(item=> item.text === noteName)
+//     if (indexItem !== -1){
+//      return this.items.splice(indexItem,1)}
+//     }
+  
+//   updatePriority(text, newPriority){
+//    const indexItem =this.items.findIndex(item=> item.text === text)
+//    if (indexItem !== -1){
+//     this.items[indexItem].priority = newPriority;
+//    }
+//   }
+// }
+// const notes =new Notes();
+// notes.addNote({text:"Завдання", priority: Notes.Priority.high});
+// notes.addNote({text:"Завдання2", priority:Notes.Priority.low});
+// notes.addNote({text:"Завдання3", priority: Notes.Priority.high});
+// notes.showNotes();
+// notes.removeNote("Завдання");
+// notes.showNotes();
+// notes.updatePriority("Завдання2", Notes.Priority.high);
+// notes.showNotes();
+
+//TODO: на class ======================
+//  Написати клас, який буде представляти зоопарк тварин.
+//  Створити загальний клас Animal для тварин.
+// У зоопарку повинні бути різні типи тварин, такі як ссавці, птахи, рептилії тощо.
+// Кожен тип тварин повинен мати свої властивості та методи, наприклад,
+//  методи для отримання інформації про тварину та для годівлі тварин.
+//  Крім того, зоопарк повинен мати метод для додавання тварин до списку та
+// метод для виведення списку всіх тварин у зоопарку.
