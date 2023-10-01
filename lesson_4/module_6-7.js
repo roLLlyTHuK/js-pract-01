82c6e8fbee172a4861d480a866ebbec2a4b50f4a
bonny-art <svitlana.otenko@gmail.com>
2023-10-01 19:30:26 +0300

    task03

--- lesson_4/module_6-7.js
+++ lesson_4/module_6-7.js
@@ -39,3 +39,23 @@
 // }
 
 // changeBtn.addEventListener("click", changeText);
+
+//TODO: № 3 ==============================================
+// Кнопка "Приховати" ховає текст і замінює назву кнопки на
+// "Розкрити", при повторному натисканні текст знову стає доступним
+// і кнопка набуває початкового вигляду.
+
+// const button = document.querySelector("#passwordButton");
+// const input = document.querySelector("#passwordInput");
+
+// button.addEventListener("click", onClick);
+
+// function onClick() {
+//   if (input.type === "text") {
+//     input.setAttribute("type", "password");
+//     button.textContent = "Розкрити";
+//   } else {
+//     input.setAttribute("type", "text");
+//     button.textContent = "Приховати";
+//   }
+// }
