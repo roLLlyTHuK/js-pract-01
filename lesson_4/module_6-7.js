86255f8ee1e155afc39cb3cc05c13e5865c20b6d
bonny-art <svitlana.otenko@gmail.com>
2023-10-01 18:53:23 +0300

    task01

---
+++ lesson_4/module_6-7.js
@@ -0,0 +1,11 @@
+//TODO: № 1 ==============================================
+// Натискання на кнопку "SHOW ME" має виводити значення з поля введення (дивіться на елементи в html-розмітці)
+
+// const button = document.querySelector("#alertButton");
+// const input = document.querySelector("#alertInput");
+
+// button.addEventListener("click", onClick);
+
+// function onClick() {
+//   console.log(input.value);
+// }
