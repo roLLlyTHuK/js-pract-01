d10be0cd857c59f9c61936c68067617b4bd42ca0
IvanImen <134809500+IvanImen@users.noreply.github.com>
2023-10-01 19:08:32 +0300

    second_task

--- lesson_4/module_6-7.js
+++ lesson_4/module_6-7.js
@@ -9,3 +9,19 @@
 // function onClick() {
 //   console.log(input.value);
 // }
+
+//TODO: № 2 ==============================================
+// Після натискання кнопки "SWAP ME" здійснюється обмін вмістом між двома інпутами.
+// Ви можете натиснути на неї кілька разів або вручну змінити вміст інпутів.
+
+const button = document.querySelector("#swapButton");
+const first_input = document.querySelector("#leftSwapInput");
+const second_input = document.querySelector("#rightSwapInput");
+
+button.addEventListener("click", onClick);
+
+function onClick() {
+    let swap = first_input.value;
+    first_input.value = second_input.value;
+    second_input.value = swap;   
+}
